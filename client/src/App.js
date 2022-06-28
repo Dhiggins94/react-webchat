510e10924bfe4c31e23121b68faa802129c03e21
Divine Higgins <dhiggins94@gmail.com>
2022-06-28 00:53:10 -0400

    added codemod for mui styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,44 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
 import { Typography, AppBar } from "@mui/material";
 import VideoPlayer from "./components/VideoPlayer";
 import Notifications from "./components/Notifications";
 import Options from "./components/Options";
+const PREFIX = "App";
+
+const classes = {
+  appBar: `${PREFIX}-appBar`,
+  wrapper: `${PREFIX}-wrapper`,
+};
+
+const Root = styled("div")(({ theme }) => ({
+  [`& .${classes.appBar}`]: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+
+  [`&.${classes.wrapper}`]: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
+
 const App = () => {
   return (
-    <div>
-      <AppBar position="static" color="inherit">
+    <Root className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography variant="h2" align="center">
           Video Chat
         </Typography>
@@ -15,7 +47,7 @@ const App = () => {
       <Options>
         <Notifications />
       </Options>
-    </div>
+    </Root>
   );
 };
 
